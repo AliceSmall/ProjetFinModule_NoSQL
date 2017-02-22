cb859ffcfc486790c3dbec5357d3a50ee110868d
AliceSmall <andreaknrtzr@aol.com>
2017-02-22 23:11:15 +0100

    Add files via upload

---
+++ import.sh
@@ -0,0 +1,3 @@
+mongoimport -d nyc -c subways --type csv --file subways.csv --headerline
+
+mongoimport -d nyc -c parks --type csv --file parks.csv --headerline
---
+++ telechargement.sh
@@ -0,0 +1,3 @@
+wget -O subways.csv https://data.cityofnewyork.us/api/views/kk4q-3rt2/rows.csv?accessType=DOWNLOAD
+
+wget -O parks.csv https://data.cityofnewyork.us/api/views/y6ja-fw4f/rows.csv?accessType=DOWNLOAD
